ed13425aa65b26b81188f023d20c8d1f61abb157
MARCO DI CARLO <48115731@ort>
2024-08-01 13:34:03 -0300

    ss

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Tp_MVC_DI_Carlo.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
